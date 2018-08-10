13aae16f56639cc1fa183da584e3fe086ae8c997
Rashid Khan <github.fliplap@spamgourmet.com>
2018-08-10 16:56:00 -0700

    IT WORKS

---
+++ commands/random.js
@@ -0,0 +1,18 @@
+import { random } from 'lodash';
+
+export default () => ({
+  help: 'Generate a random number within a range.',
+  example: '0 1',
+  fn: args => {
+    const range = args.split(' ');
+    const lower = Number(range[0]);
+    const upper = Number(range[1]);
+
+    if (Number.isNaN(lower) || Number.isNaN(upper))
+      throw new Error('upper and lower bounds are required');
+
+    return new Promise(resolve => {
+      resolve(random(upper, lower));
+    });
+  },
+});
