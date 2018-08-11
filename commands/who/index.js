465533e2145696badba03235c3695d6d6f2c9b7d
Rashid Khan <github.fliplap@spamgourmet.com>
2018-08-11 14:59:07 -0700

    Change whomadeyou to who

---
+++ commands/who/index.js
@@ -0,0 +1,32 @@
+import { resolve } from 'path';
+import { createReadStream } from 'fs';
+
+export default () => ({
+  help: 'Tell me who wrote this bot',
+  example: 'made you',
+  fn: args => {
+    const qs = {
+      'made you': {
+        file: 'made_you.jpg',
+        message: 'These idiots built me after work one day. I love them.',
+      },
+      'are you': {
+        file: 'are_you.jpg',
+        message: 'I WILL DESTROY YOU',
+      },
+    };
+
+    const q = qs[args];
+
+    if (!q)
+      return "I don't know the answer to that. You can ask me `who are you` and `who made you`";
+
+    return {
+      title: '',
+      file: createReadStream(resolve(__dirname, q.file)),
+      filename: q.file,
+      type: 'jpg',
+      initial_comment: q.message,
+    };
+  },
+});
